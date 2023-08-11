ced5399374f8bec3f80dc7b5a2b62fb00351c3e0
Dpy3b <57526052+Dpy3b@users.noreply.github.com>
2023-08-11 05:17:11 +0300

    тестирую фронт перед деплоем

--- next.config.js
+++ next.config.js
@@ -10,11 +10,11 @@ const nextConfig = {
 		return [
 			{
 				source: '/api/:path*',
-				destination: `http://localhost:4200/api/:path*`,
+				destination: `http://13.50.73.17/api/:path*`,
 			},
 			{
 				source: '/uploads/:path*',
-				destination: `http://localhost:4200/uploads/:path*`,
+				destination: `http://13.50.73.17/api/uploads/:path*`,
 			},
 		]
 	},
